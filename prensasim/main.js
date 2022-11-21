7a95e407c1871cbc365ac4198cec3b2a80bde006
Luis Matheus <luismatheusbs.2016@gmail.com>
2022-11-21 23:34:03 +0000

    Adicionado slider

--- prensasim/main.js
+++ prensasim/main.js
@@ -1,6 +1,17 @@
 import { Fluidos } from './model/Fluidos.js';
 
 
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = (() => {
+  let value = inputSlider.value;
+  slideValue.textContent = value;
+  slideValue.style.left = (value / 2) + "%";
+  slideValue.classList.add("show");
+});
+inputSlider.onblur = (() => {
+  slideValue.classList.remove("show");
+});
 
 //const forca = parseInt(prompt("Digite a força que será aplicada a viga (N)"));
 
