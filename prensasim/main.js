bf5f7f97f8511d6eca108e1353c6f3cbba2080dc
Luis Matheus <luismatheusbs.2016@gmail.com>
2022-11-24 22:47:50 +0000

    Segmentando Ferramentas

--- prensasim/main.js
+++ prensasim/main.js
@@ -1,8 +1,26 @@
 import { Fluidos } from './model/Fluidos.js';
 
 
+const escolhaContainer = document.querySelector("#escolha"); 
+const btnBomba = document.querySelector(".btn-bomba");
+const btnPrensa = document.querySelector(".btn-prensa");
+
+//Prensa
+const prensaControls = document.querySelector("#prensa-controls");
+const prensaImage = document.querySelector(".prensa");
 const slideValue = document.querySelector("span");
 const inputSlider = document.querySelector("input");
+
+prensaControls.classList.add(" hidden");
+prensaImage.classList.add("hidden");
+
+btnPrensa.addEventListener("click", (e)=> {
+  
+  prensaControls.classList.toggle("hidden");
+  prensaImage.classList.toggle("hidden");
+
+})
+
 inputSlider.oninput = (() => {
   let value = inputSlider.value;
   slideValue.textContent = value;
